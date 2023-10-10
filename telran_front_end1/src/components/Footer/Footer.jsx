9fd2a2a16a56d18396c7f28ae927b03471531753
EXHVUSTED <nikitaminecraft.90@gmail.com>
2023-10-10 14:11:14 +0200

    1

---
+++ telran_front_end1/src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <h1>Contact</h1>
+        <h1>+49 999 999 99 99</h1>
+        <h1>
+          Address
+        </h1>
+        <h1>
+          <a href='https://www.google.com/search?q=telranDE'>Linkstraße 2, 8 OG, 10785,<br></br>
+          Berlin, Deutschland</a>
+        </h1>
+        <div>
+          Working Hours:
+        <b>
+          24 hours a day
+        </b>
+        <br></br>        
+        <a href='https://www.instagram.com'>Instagram </a>
+        <a href='https://web.whatsapp.com'>WhatsApp</a>
+        </div>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9713.636886541603!2d13.3750447!3d52.5079329!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8515353a68755%3A0xd0866511db4f838f!2sStarta%20Institute%20by%20Tel-Ran!5e0!3m2!1sru!2sde!4v1696870268215!5m2!1sru!2sde" width="1345" height="525"></iframe>
+      </div>
+    </footer>
+  )
+}
